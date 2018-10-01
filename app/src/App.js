f4b841bf0ab9fe277ffbad56f74f22c98a9e150c
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-01 19:59:02 +0530

    Add Routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LoginComponent from './componenets/LoginComponent';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoginComponent from "./componenets/LoginComponent";
+import RegisterComponent from "./componenets/RegisterComponent";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <LoginComponent />
+        <BrowserRouter>
+          <div>
+            <Route path="/login" component={LoginComponent} />
+            <Route path="/register" component={RegisterComponent} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
