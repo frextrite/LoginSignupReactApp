fea7c645d24ac71ca0ad16c9971f2de5d567a105
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-01 19:24:35 +0530

    Update Login Component

--- app/src/componenets/LoginComponent.jsx
+++ app/src/componenets/LoginComponent.jsx
@@ -2,16 +2,55 @@ import React, { Component } from "react";
 
 class LoginComponent extends Component {
   state = {};
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    console.log({ [name]: value });
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div>
-        <h1>Login</h1>
-        <form id="login">
-          <label htmlFor="username">Username: </label>
-          <input type="text" name="username" />
-          <label htmlFor="password">Password: </label>
-          <input type="password" name="password" />
-          <input type="submit"></input>
+        <form className="login-form m-2">
+          <h1>Welcome</h1>
+          <fieldset>
+            <label htmlFor="username">Username</label>
+            <br />
+            <input
+              id="username"
+              className="mb-2 form-control-sm"
+              type="text"
+              name="username"
+              placeholder="Enter username"
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              id="password"
+              className="mb-2 form-control-sm"
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={this.handleChange}
+            />
+            <br />
+            <p className="small">Not Registered? <a href="#">Create an account</a></p>
+            <button
+              className="btn btn-primary"
+              id="submit"
+              onClick={this.handleSubmit}
+            >
+              Login
+            </button>
+          </fieldset>
         </form>
       </div>
     );
