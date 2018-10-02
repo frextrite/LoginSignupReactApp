0a7a8459ca2c1e6a623a577e3086b2e996159b0a
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-02 13:02:13 +0530

    Add styling to register component

--- app/src/componenets/RegisterComponent.jsx
+++ app/src/componenets/RegisterComponent.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./RegisterComponent.css";
 
 class RegisterComponent extends Component {
   state = {};
@@ -17,69 +18,89 @@ class RegisterComponent extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Sign up</h2>
-        <form className="register-form m-2" onSubmit={this.handleSubmit}>
-          <fieldset>
-            <label htmlFor="firstname">First Name</label>
-            <br />
-            <input
-              id="firstname"
-              className="mb-2 form-control-sm"
-              type="text"
-              name="firstname"
-              placeholder="Enter first name"
-              onChange={this.handleChange}
-            />
-            <br />
+      <div className="register">
+        <div className="wrap-register">
+          <form className="register-form m-2" onSubmit={this.handleSubmit}>
+            <h2 className="form-head">Sign up</h2>
+            <fieldset>
+              <div className="wrap-label-input">
+                <input
+                  id="firstname"
+                  className="input-field"
+                  type="text"
+                  name="firstname"
+                  onChange={this.handleChange}
+                  required
+                />
+                <br />
+                <label className="label-field" htmlFor="firstname">
+                  First Name
+                </label>
+                <br />
+              </div>
 
-            <label htmlFor="lastname">Last Name</label>
-            <br />
-            <input
-              id="lastname"
-              className="mb-2 form-control-sm"
-              type="text"
-              name="lastname"
-              placeholder="Enter last name"
-              onChange={this.handleChange}
-            />
-            <br />
+              <div className="wrap-label-input">
+                <input
+                  id="lastname"
+                  className="input-field"
+                  type="text"
+                  name="lastname"
+                  onChange={this.handleChange}
+                  required
+                />
+                <br />
+                <label className="label-field" htmlFor="lastname">
+                  Last Name
+                </label>
+                <br />
+              </div>
 
-            <label htmlFor="username">Username</label>
-            <br />
-            <input
-              id="username"
-              className="mb-2 form-control-sm"
-              type="text"
-              name="username"
-              placeholder="Enter username"
-              onChange={this.handleChange}
-            />
-            <br />
+              <div className="wrap-label-input">
+                <input
+                  id="username"
+                  className="input-field"
+                  type="text"
+                  name="username"
+                  onChange={this.handleChange}
+                  required
+                />
+                <br />
+                <label className="label-field" htmlFor="username">
+                  Username
+                </label>
+                <br />
+              </div>
 
-            <label htmlFor="password">Password</label>
-            <br />
-            <input
-              id="password"
-              className="mb-2 form-control-sm"
-              type="password"
-              name="password"
-              placeholder="Enter password"
-              onChange={this.handleChange}
-            />
-            <br />
+              <div className="wrap-label-input">
+                <input
+                  id="password"
+                  className="input-field"
+                  type="password"
+                  name="password"
+                  onChange={this.handleChange}
+                  required
+                />
+                <br />
+                <label className="label-field" htmlFor="password">
+                  Password
+                </label>
+                <br />
+              </div>
 
-            <button
-              className="btn btn-primary"
-              id="submit"
-              onClick={this.handleSubmit}
-            >
-              Sign up
-            </button>
+              <button
+                className="signup-btn btn btn-primary"
+                id="submit"
+                onClick={this.handleSubmit}
+              >
+                Sign up
+              </button>
 
-              <Link className="btn btn-link" to="/login">Login</Link>
-          </fieldset>
-        </form>
+              <Link className="login-btn btn btn-link" to="/login">
+                Login
+              </Link>
+            </fieldset>
+          </form>
+        </div>
       </div>
     );
   }
