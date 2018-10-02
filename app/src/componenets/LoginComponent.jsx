e793b0ebcfc593648525acb67b58f1deeacd55c4
Amol Grover <29180487+frextrite@users.noreply.github.com>
2018-10-02 13:01:58 +0530

    Update styling for login component

--- app/src/componenets/LoginComponent.jsx
+++ app/src/componenets/LoginComponent.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./LoginComponent.css";
 
 class LoginComponent extends Component {
   state = {};
@@ -17,42 +18,51 @@ class LoginComponent extends Component {
 
   render() {
     return (
-      <div>
-        <form className="login-form m-2">
-          <h1>Welcome</h1>
-          <fieldset>
-            <label htmlFor="username">Username</label>
-            <br />
-            <input
-              id="username"
-              className="mb-2 form-control-sm"
-              type="text"
-              name="username"
-              placeholder="Enter username"
-              onChange={this.handleChange}
-            />
-            <br />
-            <label htmlFor="password">Password</label>
-            <br />
-            <input
-              id="password"
-              className="mb-2 form-control-sm"
-              type="password"
-              name="password"
-              placeholder="Enter password"
-              onChange={this.handleChange}
-            />
-            <br />
-            <p className="small">Not Registered? <Link to="/register">Create an account</Link></p>
-            <button
-              className="btn btn-primary"
-              id="submit"
-              onClick={this.handleSubmit}
-            >
-              Login
-            </button>
-          </fieldset>
-        </form>
+      <div className="login">
+        <div className="wrap-login">
+          <form className="login-form m-2">
+            <h1 className="form-head">Welcome</h1>
+            <fieldset>
+              <div className="wrap-username">
+                <input
+                  className="input-field username"
+                  type="text"
+                  name="username"
+                  onChange={this.handleChange}
+                  required
+                  />
+                <br />
+                <label className="label-field" htmlFor="username">
+                  Username
+                </label>
+                <br />
+              </div>
+              <div className="wrap-password">
+                <input
+                  id="password"
+                  className="input-field password"
+                  type="password"
+                  name="password"
+                  onChange={this.handleChange}
+                  required
+                />
+                <br />
+                <label className="label-field" htmlFor="password">Password</label>
+                <br />
+              </div>
+              <button
+                className="login-btn btn btn-primary"
+                id="submit"
+                onClick={this.handleSubmit}
+              >
+                Login
+              </button>
+              <p className="small">
+                Not Registered? <Link to="/register">Create an account</Link>
+              </p>
+            </fieldset>
+          </form>
+        </div>
       </div>
     );
   }
